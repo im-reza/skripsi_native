ace9e7a2420ea85769972e08d2107469b17ccdf5
mreza18 <reza18sep@gmail.com>
2020-05-31 06:51:59 +0700

    ubah laporan disposisi perdata

--- admin/laporan/lap_data_ds.php
+++ admin/laporan/lap_data_ds.php
@@ -1,18 +1,21 @@
-<?php include_once '../../assets/kop_surat.php'; include '../../connections/connection_db.php';?>
-<?php  
-$id=$_GET['no_surat_show'];
-$sql=mysqli_query($con,"select * from disposisi inner join surat_masuk inner join file on surat_masuk.no_surat=disposisi.no_surat and surat_masuk.no_surat=file.no_surat where disposisi.no_surat='$id';");
-while ($d=mysqli_fetch_array($sql)) {
-	$checked[]=$d['penerima'];
-	$tgl_masuk=date('d-m-Y',strtotime($d['tgl_masuk']));
-	$tgl_surat=date('d-m-Y',strtotime($d['tgl_surat']));
-	$tgl_verif=date('d-m-Y H:i',strtotime($d['tgl']));
-	$tgl_ttd=date('d-m-Y',strtotime($d['tgl']));
+<?php include_once '../../assets/kop_surat.php';
+include '../../connections/connection_db.php'; ?>
+<?php
+$id = $_GET['no_surat_show'];
+$sql = mysqli_query($con, "select surat_masuk.no_surat,surat_masuk.tgl_surat,surat_masuk.pengirim,surat_masuk.perihal,file.nama_file,file.tgl_masuk,disposisi.catatan,disposisi.tgl, GROUP_CONCAT(penerima SEPARATOR  ' & ') as penerima from disposisi inner join surat_masuk inner join file on surat_masuk.no_surat=disposisi.no_surat and surat_masuk.no_surat=file.no_surat where disposisi.no_surat='$id' GROUP BY disposisi.no_surat order by disposisi.tgl;");
+while ($d = mysqli_fetch_array($sql)) {
+	$tgl_masuk = date('d-m-Y', strtotime($d['tgl_masuk']));
+	$tgl_surat = date('d-m-Y', strtotime($d['tgl_surat']));
+	$tgl_verif = date('d-m-Y H:i', strtotime($d['tgl']));
+	$tgl_ttd = date('d-m-Y', strtotime($d['tgl']));
 	?>
 	<div class="row">
 		<div class="col-md-12">
 			<center>
-				<u><h3>LEMBAR DISPOSISI</h3></u>
+				<u>
+					<h3>LEMBAR DISPOSISI</h3>
+				</u>
+				<h5>No Surat : <?php echo $d['no_surat'] ?></h5>
 			</center>
 		</div>
 	</div>
@@ -41,18 +44,9 @@ while ($d=mysqli_fetch_array($sql)) {
 						</thead>
 					</table>
 					<label style="margin-top: 4%"><strong>Kepada Sodara :</strong></label>
-						<div class="form-check">
-							<input type="checkbox" class="form-check-input" value="admin" <?php if (in_array('admin', $checked)) echo "checked : '' "; ?>>
-							<label class="form-check-label">admin</label>
-						</div>
-						<div class="form-check">
-							<input type="checkbox" class="form-check-input" value="kabag" <?php if (in_array('kabag', $checked)) echo "checked : '' "; ?>>
-							<label class="form-check-label">kabag</label>
-						</div>
-						<div class="form-check">
-							<input type="checkbox" class="form-check-input" value="kasubag otda" <?php if ($checked=='kasubag otda'){echo "checked : ''";} ?>>
-							<label class="form-check-label">kasubag otda</label>
-						</div>
+					<div class="form-group">
+						<input type="text" class="form-control" value="<?php echo $d['penerima'] ?>">
+					</div>
 					<div class="form-group" style="margin-top: 4%">
 						<label><strong>Catatan :</strong></label>
 						<textarea class="form-control" style="font-size: larger; color: blue"><?php echo $d['catatan']; ?></textarea>
@@ -69,12 +63,12 @@ while ($d=mysqli_fetch_array($sql)) {
 	<div class="col-md-2"></div>
 	<div class="col-md-4" style="text-align: center; font-size: 14px;font-family: sans-serif;margin-top: 1%;">
 		<b>
-			<?php  include '../../connections/connection_db.php';
+			<?php include '../../connections/connection_db.php';
 			date_default_timezone_set('Asia/Jakarta');
-			$query=mysqli_query($con,"select * from user where name='kabag'");
-			$user=mysqli_fetch_array($query);
-			$ttd=$user['nama'];
-			$nip=$user['nip'];
+			$query = mysqli_query($con, "select * from user where name='kabag'");
+			$user = mysqli_fetch_array($query);
+			$ttd = $user['nama'];
+			$nip = $user['nip'];
 
 			?>
 			<p>Banjarmasin, <?php echo $tgl_ttd ?> </p>
@@ -88,4 +82,5 @@ while ($d=mysqli_fetch_array($sql)) {
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 </body>
+
 </html>
